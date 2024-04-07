6b21f08d128ad0f7ffe03668d47048709a0ec512
Bilal Ali <bilalal495@outlook.com>
2024-04-07 03:15:15 -0400

    Define request struct

---
+++ http_server/include/http_request.h
@@ -0,0 +1,23 @@
+#ifndef HTTP_REQUEST_H
+#define HTTP_REQUEST_H
+
+#include <string>
+
+struct HttpRequest {
+    // Request line
+    std::string req_method;         // HTTP method (GET, POST, ...)
+    std::string path;               // request URL
+    std::string version;            // HTTP version (HTTP/1.1)
+
+    // Headers
+
+    // Body
+    std::string body;
+
+    // TODO add field for session
+
+    // TODO add method to clear request fields
+    // useful for after a transaction is complete 
+};
+
+#endif
