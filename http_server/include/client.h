dee7da663b94cd6c0542fa4be877bd4405cb586c
Bilal Ali <bilalal495@outlook.com>
2024-04-07 16:28:54 -0400

    Parse req line from client

--- http_server/include/client.h
+++ http_server/include/client.h
@@ -10,9 +10,14 @@ class Client
 {
 // fields
 public:
+    static const std::string CRLF;
     static const std::string DOUBLE_CRLF;
-    HttpRequest req;
-    HttpResponse res;
+    // note that requests/responses for a client are sequential, so after a response is sent, both req and res must be cleared
+    HttpRequest m_req;    // current request 
+    HttpResponse m_res;   // response for current request
+    bool response_ready;   // tracks if a response was sent
+    bool remaining_body_len;
+    bool close_connection = false;
 
 private:
     int m_client_fd;        // client's bound fd
@@ -25,6 +30,13 @@ public:
     Client() = delete; 
 
     void read_from_network();   // run server
+
+private:
+    void handle_req(std::string& client_stream);
+    void parse_req_line(std::string& req_line);
+    void parse_headers(std::vector<std::string> headers);
+    void validate_request();
+    void construct_error_response(int err_code);
 };
 
 #endif
--- http_server/src/client.cc
+++ http_server/src/client.cc
@@ -1,46 +1,31 @@
 #include <iostream>
 #include <sys/socket.h>   // recv
 #include <unistd.h>       // close
-#include <sstream>        // istringstream
 
 #include "client.h"
 #include "http_request.h"
+#include "http_server.h"
 #include "utils.h"
 
+const std::string Client::CRLF = "\r\n";
 const std::string Client::DOUBLE_CRLF = "\r\n\r\n";
 
 void Client::read_from_network()
 {
-    // build request from client
     std::string client_stream;
     int bytes_recvd;
 
     while (true) {
-        // TODO check if std::array is okay over using char[]
-        std::array<char, 4096> buf;
-        bytes_recvd = recv(m_client_fd, buf.data(), buf.size(), 0);
-
-        // // error while reading from client
-        // if (bytes_recvd < 0) {
-        //     error("Error receiving data from client");
-        //     // TODO should we break here?
-        //     break;
-        // }
-        // // TODO I think this means the client closed the connection
-        //     else if (bytes_recvd == 0) {
-        //     break;
-        // }
-
-        // ! STRUCTURE
-        // look for double CRLF (\r\n\r\n)
-        // when you see this, parse the text and create an http request object
+        char buf[4096];
+        // ! error check recv function
+        bytes_recvd = recv(m_client_fd, buf, 4096, 0);
 
         for (int i = 0 ; i < bytes_recvd ; i++) {
             client_stream += buf[i];
-            // double CRLF at end of client stream indicates a full http request
+            // double CRLF indicates a full http request WITHOUT the body (if present)
             if (client_stream.length() >= 4 && client_stream.substr(client_stream.length() - 4) == DOUBLE_CRLF) {
-                // build http request from client stream and then clear stream
-                HttpRequest req = parse_stream_for_req(client_stream);
+                // build http request from client stream and then clear current stream
+                handle_req(client_stream);
                 client_stream.clear();
             }
         }
@@ -48,28 +33,94 @@ void Client::read_from_network()
     close(m_client_fd);
 }
 
-HttpRequest parse_stream_for_req(std::string& client_stream) {
-    HttpRequest req;
-    std::string delimiter = "\r\n";
-    std::vector<std::string> msg_lines = Utils::parse_string(client_stream, delimiter);
 
-    // first line is always the request line
-    std::vector<std::string> req_line = Utils::parse_string(msg_lines.at(0), " ");
-    // verify req_line has 3 values
-    if (req_line.size() != 3) {
-        // ! write error message
-    }
-    req.req_method = req_line.at(0);
-    req.path = req_line.at(1);
-    req.version = req_line.at(2);
+void Client::handle_req(std::string& client_stream) {
+    std::vector<std::string> msg_lines = Utils::split(client_stream, CRLF);
+    parse_req_line(msg_lines.at(0));
+    msg_lines.erase(msg_lines.begin());
+    parse_headers(msg_lines);
+    validate_request();
 
-    // all remaining lines in msg_lines header lines
-    for (auto it = msg_lines.begin() + 1; it != msg_lines.end(); it++) {
-        // verify string is of non-zero length before parsing header
-        if ((*it).length() != 0) {
+    // check if the request has a body
+    // if so, set the remaining body length value for the client
 
+    auto content_len_it = m_req.headers.find("content_length");
+    if (content_len_it != m_req.headers.end()) {
+        if ((content_len_it->second).size() > 1) {
+            construct_error_response(400);
+        } else if ((content_len_it->second).size() == 1) {
+            try {
+                remaining_body_len = std::stoi((content_len_it->second).at(0));
+            } catch (const std::exception& e) {
+                construct_error_response(400);
+            }
         }
     }
-    
-    return req;
+
+    // handle session related headers too i think
+
+
+
+    // check if the client requested to close the connection
+    // if so, set close_connection to true
+    auto connection_it = m_req.headers.find("connection");
+    if (connection_it != m_req.headers.end()) {
+        if ((connection_it->second).size() > 1) {
+            construct_error_response(400);
+        } else if ((connection_it->second).size() == 1 && (connection_it->second).at(0) == "close") {
+            close_connection = true;
+        }
+    }
 }
+
+
+void Client::parse_req_line(std::string& req_line)
+{
+    // verify req_line has 3 values
+    if (req_line.size() != 3) {
+        construct_error_response(400);
+    }
+    m_req.req_method = req_line.at(0);
+    m_req.path = req_line.at(1);
+    m_req.version = req_line.at(2);
+}
+
+
+void Client::parse_headers(std::vector<std::string> headers)
+{
+    for (std::string header : headers) {
+        // split header
+        std::vector<std::string> header_components = Utils::split_on_first_delim(header, ":");
+        if (header_components.size() != 2) {
+            construct_error_response(400);
+        }
+        std::string header_key = Utils::to_lowercase(header_components.at(0));
+        std::string header_values = Utils::trim(header_components.at(1));
+
+        // ! store headers in m_req
+    }
+}
+
+
+void Client::construct_error_response(int err_code)
+{
+    // response is ready to send back to client
+    response_ready = true;
+}
+
+
+// void Client::validate_request()
+// {
+//     // host headers not present in parsed headers
+
+
+//     // unsupported http version
+//     if (m_req.version != HttpServer::version) {
+
+//     }
+
+//     // unsupported method
+//     if (m_req.version != HttpServer::version) {
+
+//     }
+// }
