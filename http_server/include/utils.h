88cac8970affd428673db685095a087197441073
Bilal Ali <bilalal495@outlook.com>
2024-04-07 13:59:40 -0400

    Add string utility functions

--- http_server/include/utils.h
+++ http_server/include/utils.h
@@ -6,10 +6,25 @@
 
 namespace Utils
 {
-    std::vector<std::string> parse_string(std::string s, std::string delimiter);
+    // split string by delimiter
+    std::vector<std::string> split(std::string s, std::string delimiter);
 
+    // trim whitespace from left side of string
+    std::string l_trim(std::string s);
+
+    // trim whitespace from right side of string
+    std::string r_trim(std::string s);
+
+    // trim whitespace from string on both sides
     std::string trim(std::string s);
 
+    // convert string to uppercase
+    std::string to_uppercase(std::string s);
+
+    // convert string to lowercase
+    std::string to_lowercase(std::string s);
+
+    // ! this will be replaced by logger
     void error(std::string msg);
 }
 
--- http_server/utils.cc
+++ http_server/utils.cc
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <algorithm>    // std::transform
 
 #include "utils.h"
 
-std::vector<std::string> Utils::parse_string(std::string s, std::string delimiter) 
+std::vector<std::string> Utils::split(std::string s, std::string delimiter) 
 {
     std::vector<std::string> lines;
     size_t pos = 0;
@@ -23,11 +24,50 @@ std::vector<std::string> Utils::parse_string(std::string s, std::string delimite
     return lines;
 }
 
-// ! trim s
+
+std::string Utils::l_trim(std::string s)
+{
+    size_t i = 0;
+    while (i < s.length() && std::isspace(s[i])) {
+        i++;
+    }
+    return s.substr(i);
+}
+
+
+std::string Utils::r_trim(std::string s)
+{
+    size_t i = s.length() - 1;
+    while (i >= 0 && std::isspace(s[i])) {
+        i--;
+    }
+    return s.substr(0, i + 1);
+}
+
+
 std::string Utils::trim(std::string s)
 {
-
+    return r_trim(l_trim(s));
 }
+
+
+std::string to_uppercase(std::string s) 
+{   
+    for (char& c : s) {
+        c = std::toupper(c);
+    }
+    return s;
+}
+
+
+std::string to_lowercase(std::string s)
+{
+    for (char& c : s) {
+        c = std::tolower(c);
+    }
+    return s;
+}
+
 
 void Utils::error(std::string msg) {
     std::cerr << "[ERROR] " << msg << std::endl;
