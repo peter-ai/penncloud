69369e02498a283fd8957c8202bc9a20e87a3b27
aashok12 <aashok@seas.upenn.edu>
2024-04-12 23:54:15 -0400

    initial commit for open_filefolder
    Signed-off-by: aashok12 <aashok@seas.upenn.edu>

--- front_end/src/drive.cc
+++ front_end/src/drive.cc
@@ -5,111 +5,116 @@
  *      Author: aashok12
  */
 
-#include "../include/drive.h"
-#include "../../http_server/include/http_server.h"
-#include "../../utils/include/utils.h"
 #include <iostream>
 #include <cstring>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <algorithm>
+
+#include "../include/drive.h"
+#include "../../http_server/include/http_server.h"
+#include "../../utils/include/utils.h"
+#include "../utils/include/fe_utils.h"
 
 // Folder handlers
 
 // @todo: check path vs parameters of request
 
-void open_folder(const HttpRequest &req, HttpResponse &res, std::string ipaddr, int port)
+std::vector<char> content_vec = {'c','o','n','t','e','n','t'};
+
+// helper to return parent path
+std::string constr_parentpath(const std::vector<std::string>& vec) {
+    std::string parentpath;
+
+    // Iterate over all elements except the last one
+    for (size_t i = 0; i < vec.size() - 1; ++i) {
+        parentpath += vec[i]; // Append the current element to the result string
+    }
+
+    return parentpath;
+}
+
+
+bool kv_successful(const std::vector<char>& vec) {
+    // Check if the vector has at least 3 characters
+    if (vec.size() < 3) {
+        return false;
+    }
+
+    // Define the expected prefix
+    std::vector<char> prefix = {'+', 'O', 'K'};
+
+    // Check if the first three characters match the prefix
+    return std::equal(prefix.begin(), prefix.end(), vec.begin());
+}
+
+void open_filefolder(const HttpRequest &req, HttpResponse &res, std::string ipaddr, int port)
 {
     // check req method
-    if (req.req_method == "GET")
+    if (req.method == "GET")
     {
-        // path is /api/drive/:dirpath
-        std::string dirpath = req.path.substr(11);
-        std::cout << "GET request received for directory: " << dirpath << std::endl;
-        const char *req_data = req.body.data();       // get raw string bytes
-        const size_t req_data_size = req.body.size(); // get size of folder name
-        std::cout << "User wants to open folder: " << std::string(req_data, req_data_size) << std::endl;
+        // path is /api/drive/:childpath where parent dir is the page that is being displayed
 
-        std::string folderpath = dirpath + "/" + std::string(req_data, req_data_size);
+        std::string childpath_str = req.path.substr(11);
+        std::vector<char> child_path (childpath_str.begin(), childpath_str.end());
 
-        // Create a TCP socket
-        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if (sockfd == -1)
-        {
-            std::cerr << "Error creating socket" << std::endl;
-            // log error here
+
+        std::cout << "GET request received for child path: " << childpath_str << std::endl;
+
+        //construct parent path string
+        std::string parentpath_str = constr_parentpath(Utils::split(childpath_str,"/"));
+
+
+        std::cout << "Parent path is: " << parentpath_str.c_str() << std::endl;
+
+        // Construct parent path vector
+        std::vector<char> parent_path(parentpath_str.begin(), parentpath_str.end());
+         
+        int sockfd = FeUtils::open_socket();
+        
+        // currently assuming options are 'f' for file or 'd' for directory
+        std::vector<char> ftype = FeUtils::kv_get(sockfd, parent_path, child_path);
+
+        if (kv_successful){
+
+        // @todo change if any meta data is added
+        if (ftype[4] == 'f'){
+            // file 
+            // follow format for file with row = path, col = 'content', val = data
+            std::vector<char> file_content = FeUtils::kv_get(sockfd, child_path, content_vec);
+            res.append_body_bytes(file_content.data(), file_content.size());
+
+            // append header for content length
+            std::string content_header = "Content-Length:";
+            std::string header_value = std::to_string(file_content.size());
+            res.set_header(content_header,header_value);
+            res.set_code(200);
+
+        } else if (ftype[4] == 'd'){
+            // this is a folder or directory
+            std::vector<char> folder_content = FeUtils::kv_get_row(sockfd, child_path);
+
+            //@todo: update with html!
+            res.append_body_bytes(folder_content.data(), folder_content.size());
+
+            // append header for content length
+            std::string content_header = "Content-Length:";
+            std::string header_value = std::to_string(folder_content.size());
+            res.set_header(content_header,header_value);
+            res.set_code(200);
+
         }
 
-       
-        // Fill in the server address struct
-        struct sockaddr_in server_addr;
-        memset(&server_addr, 0, sizeof(server_addr));
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_port = htons(port);
-        inet_pton(AF_INET, ipaddr.c_str(), &server_addr.sin_addr);
-
-        // Connect to the server
-        if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
-        {
-            std::cerr << "Error connecting to server" << std::endl;
-            close(sockfd);
+        } else {
+           res.set_code(400);
         }
-
-        // String to send
-        std::string message = "Hello, server!";
-
-        // Send the string
-        ssize_t bytes_sent = send(sockfd, message.c_str(), message.length(), 0);
-        if (bytes_sent == -1)
-        {
-            std::cerr << "Error sending data" << std::endl;
-            close(sockfd);
-        }
-
-        std::cout << "Sent message: " << message << std::endl;
-
-        // Close the socket
-        close(sockfd);
-
-
-        // // @note: assuming path is current dir path, same as row
-        // // send get(dirpath,folderpath) to backend
-        // // rowhash = get (....)
-        // if (rowhash == ""){
-        //     // value of get is the hash of the row that this lives on
-        //     error in folder
-        //     call logger
-        //     return;
-        // }
-
-        // // using value, get value from rowhash, index -- @todo assuming all rows have index list?
-        // // @note: maybe we can have a getcols(row) for backend which will get get col names for given row?
-        // collist = get(rowhash, index);  //can we always guarantee, or need a check?
-
-        // // parse collist for all headers
-        // // @todo: some parse utility function ()
-
-        std::vector<char> resp_body;
-
-        // connect to backend
-        // get(path,constructedpath)
-
-        // Set appropriate response code and message
-        res.code = 200;
-        res.reason = "OK";
-        res.body = resp_body;
-
-        // Your logic to set response body, headers, etc. goes here
-        // For simplicity, let's leave the response body empty
     }
     else
     {
         // If the request method is not GET, set an appropriate error response
-        res.code = 405; // Method Not Allowed
-        res.reason = "Method Not Allowed";
-        const char *message = "Only GET method is allowed for file retrieval.";
-        std::vector<char> resp_body(message, message + strlen(message));
-        res.body = resp_body;
+        res.set_code(405); // Method Not Allowed
+        std::cerr << "Only GET method is allowed for file retrieval." << std::endl;
     }
 }
