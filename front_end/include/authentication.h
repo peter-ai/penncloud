468bd1307a04f6ae52ed2a4b3801d7391f1150d6
Peter Akioyamen <peter.akioyamen@gmail.com>
2024-04-12 20:56:28 -0400

    almost completed login handler

--- front_end/include/authentication.h
+++ front_end/include/authentication.h
@@ -34,14 +34,24 @@ void logout_handler(const HttpRequest& req, HttpResponse& res);
 // function to handle password update requests
 void update_password_handler(const HttpRequest& req, HttpResponse& res);
 
+// helper function that validates the password of a user 
+// using a cryptographically secure challenge-response protocol
+bool validate_password(int kvs_fd, std::string& username, std::string& password);
+
 // helper function that validates sessionID of a user
-bool validate_session_ID(std::string& username, int sid);
+std::string validate_session_id(int kvs_fd, std::string& username, const HttpRequest& req);
+
+// helper function that parsing cookie header responses from request objects
+std::unordered_map<std::string, std::string> parse_cookies(std::vector<std::string>& cookies_vector);
 
 // helper function for hashing using SHA256 algorithm
 void sha256(char *string, char outputBuffer[65]);
 
 // helper function that generates random strings
 std::string generate_challenge(std::size_t length);
+
+// helper function that generates secure random session IDs
+std::string generate_sid();
 
 
 #endif /* FRONT_END_INCLUDE_AUTHENTICATION_H_ */
