8838283f6a73e2a14b73b4cf53172f4035d0b627
Peter Akioyamen <peter.akioyamen@gmail.com>
2024-04-12 20:51:24 -0400

    testing routes

--- example_frontend/src/routes.cc
+++ example_frontend/src/routes.cc
@@ -8,12 +8,20 @@ void test_route(const HttpRequest& req, HttpResponse& res) {
     res.append_body_str("dynamic route working!");
     res.set_header("Content-Type", "text/plain");
     res.set_header("Content-Length", res.body_size());
+    res.set_cookie("user", "me");
+    res.set_cookie("sid", "123");
+
+    // std::vector<std::string> cookies = req.get_header("Cookie"); 
+    // for (int i=0; i < cookies.size(); i++) std::cerr << "Entry #" << i+1 << " " << cookies[i] << std::endl;
+
+    // std::vector<std::string> encodings = req.get_header("Accept-Encoding"); 
+    // for (int i=0; i < encodings.size(); i++) std::cerr << "Entry #" << i+1 << " " << encodings[i] << std::endl;
 }
 
 int main()
 {
     auto test_route_handler = std::bind(&test_route, std::placeholders::_1, std::placeholders::_2);
-    HttpServer::post("/api/test", test_route_handler);
+    HttpServer::get("/api/test", test_route_handler);
     HttpServer::run(8000);
     return(0);
 }
