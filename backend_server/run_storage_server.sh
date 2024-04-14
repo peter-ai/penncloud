f6ff3d0bed7d8f309639c3cfb6d7cf940b698d46
Bilal Ali <bilalal495@outlook.com>
2024-04-13 20:01:07 -0400

    Add script to run backend main

---
+++ backend_server/run_storage_server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# make sure you're in the example_frontend directory
+
+# compile utils
+cd ../utils
+make clean
+make
+cd ..
+
+# compile routes
+cd backend_server
+make clean
+make
+./backend_main -p 5000 -s aa -e zz
