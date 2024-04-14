2879a60dcb0544e3c8631ae38521a8167f6c7597
Bilal Ali <bilalal495@outlook.com>
2024-04-13 20:18:19 -0400

    Define header file for backend server class

--- backend_server/src/backend_main.cc
+++ backend_server/src/backend_main.cc
@@ -1,11 +1,10 @@
 #include <iostream>       
 #include <unistd.h>       // getopt
-#include <sys/socket.h>   // socket
-#include <netinet/in.h>   // sockaddr_in
 
 #include "../../utils/include/utils.h"
+#include "backend_server.h"
 
-Logger kvs_logger("Storage server");
+Logger kvs_logger("Backend server");
 
 // main function to run storage server
 // main expects the following flags:
@@ -15,10 +14,6 @@ Logger kvs_logger("Storage server");
 // Example: backend_main -p 5000 -s aa -e zz
 int main(int argc, char *argv[])
 {
-    int port;
-    std::string range_start;
-    std::string range_end;
-
     kvs_logger.log("Storage server running", 20);
 
     int opt;
@@ -27,23 +22,23 @@ int main(int argc, char *argv[])
         case 'p':
             try {
                 // set storage server's port
-                port = std::stoi(optarg);
+                BackendServer::port = std::stoi(optarg);
             } catch (std::invalid_argument const& ex) {
                 kvs_logger.log("Invalid port number - exiting.", 40);
             }
             break;
         case 's':
             // assign start of tablet range managed by server
-            range_start = optarg;
-            if (range_start.length() != 2) {
+            BackendServer::range_start = optarg;
+            if (BackendServer::range_start.length() != 2) {
                 kvs_logger.log("Invalid start range - exiting", 40);
                 return(-1);
             }
             break;
         case 'e':
             // assign end of tablet range managed by server
-            range_end = optarg;
-            if (range_end.length() != 2) {
+            BackendServer::range_end = optarg;
+            if (BackendServer::range_end.length() != 2) {
                 kvs_logger.log("Invalid end range - exiting", 40);
                 return(-1);
             }
@@ -53,9 +48,10 @@ int main(int argc, char *argv[])
         }
     }
 
-    kvs_logger = Logger("KVS Server " + range_start + ":" + range_end);
-
-    kvs_logger.log("Storage server listening for connections on port " + std::to_string(port), 20);
-    kvs_logger.log("Managing key range " + range_start + ":" + range_end, 20);
+    // update logger to display start and end range managed by server
+    kvs_logger = Logger("Backend Server " + BackendServer::range_start + ":" + BackendServer::range_end);
+    // Log metadata about backend server
+    kvs_logger.log("Backend server listening for connections on port " + std::to_string(BackendServer::port), 20);
+    kvs_logger.log("Managing key range " + BackendServer::range_start + ":" + BackendServer::range_end, 20);
     return 0;
 }
