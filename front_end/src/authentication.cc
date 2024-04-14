201febd28ed3586cba813a2ff7e258aa6ef3acc1
Peter Akioyamen <peter.akioyamen@gmail.com>
2024-04-14 04:31:07 -0400

    update and test authentication route handler with POSTMAN; successfully connect to coordinator

--- front_end/src/authentication.cc
+++ front_end/src/authentication.cc
@@ -9,11 +9,13 @@
 
 // https://www.w3schools.com/tags/att_form_enctype.asp may be important once we are building upload fields for files
 
-// function to handle signup requests
+/// @brief handles new user signup requests on /api/signup route
+/// @param req HttpRequest object
+/// @param res HttpResponse object
 void signup_handler(const HttpRequest &req, HttpResponse &res)
 {
-    (void) req;
-    (void) res;
+    (void)req;
+    (void)res;
     // on signup
     // check if user exists,
     // if they do, send them to login page with special messaging
@@ -22,7 +24,9 @@ void signup_handler(const HttpRequest &req, HttpResponse &res)
     // create empty mailbox at user/ mailbox v=metadata
 }
 
-// function to handle login requests
+/// @brief handles login requests on /api/login route
+/// @param req HttpRequest object
+/// @param res HttpResponse object
 void login_handler(const HttpRequest &req, HttpResponse &res)
 {
     Logger logger("Login Handler");
@@ -35,28 +39,41 @@ void login_handler(const HttpRequest &req, HttpResponse &res)
     std::string username = Utils::trim(Utils::split(req_body[0], "=")[1]);
     std::string password = Utils::trim(Utils::split(req_body[1], "=")[1]);
     std::vector<std::string> kvs_addr;
+    bool present = HttpServer::check_kvs_addr(username);
 
     // check if we know already know the KVS server address for user
-    if (HttpServer::user_backend_address.count(username))
+    if (present)
     {
-        kvs_addr = HttpServer::user_backend_address.at(username);
+        kvs_addr = HttpServer::get_kvs_addr(username);
     }
     // otherwise get KVS server address from coordinator
     else
     {
         // query the coordinator for the KVS server address
         kvs_addr = FeUtils::query_coordinator(username);
+
+        // Testing - TODO: DELETE
+        HttpServer::set_kvs_addr(username, kvs_addr[0] + ":" + kvs_addr[1]);
     }
-    logger.log("KVS Server for " + username + " located at: <" + kvs_addr[0] + ":" + kvs_addr[1] + ">", LOGGER_INFO); // logging message
 
-    std::vector<char> buf(65);
-    std::string msg = "hi my name is";
-    sha256(&msg[0], buf.data());
-    std::cerr << buf.data() << std::endl;
-    // logger.log(, LOGGER_DEBUG);
-
+    // Testing - TODO: DELETE
     res.set_code(200);
-    res.append_body_str("Hello");
+    res.set_header("Content-Type", "text/html");
+    FeUtils::set_cookies(res, username, generate_sid());
+
+    std::string html =
+        "<!doctype html>"
+        "<html>"
+        "<head>"
+        "<title>PennCloud.com</title>"
+        "<meta name='description' content='CIS 5050 Spr24'>"
+        "<meta name='keywords' content='HomePage'>"
+        "</head>"
+        "<body>"
+        "Successful Login!"
+        "</body>"
+        "</html>";
+    res.append_body_str(html);
 
     // // create socket for communication with KVS server
     // int kvs_sock = FeUtils::open_socket(kvs_addr[0], std::stoi(kvs_addr[1]));
@@ -68,6 +85,9 @@ void login_handler(const HttpRequest &req, HttpResponse &res)
     // // if there is a valid session id, then construct response and redirect user
     // if (!valid_session_id.empty())
     // {
+    //     // if not present, set cache
+    //     if (!present) HttpServer::set_kvs_addr(username, kvs_addr[0]+":"+kvs_addr[1]);
+    //
     //     // set cookies on response
     //     FeUtils::set_cookies(res, username, valid_session_id);
     //     res.set_cookie("sid", valid_session_id);
@@ -101,6 +121,9 @@ void login_handler(const HttpRequest &req, HttpResponse &res)
 
     //     if (valid_pass)
     //     {
+    //         // if not present, set cache
+    //         if (!present) HttpServer::set_kvs_addr(username, kvs_addr[0]+":"+kvs_addr[1]);
+    //
     //         // generate random SID
     //         std::string sid = generate_sid();
 
@@ -139,27 +162,39 @@ void login_handler(const HttpRequest &req, HttpResponse &res)
     //         // set response headers
     //         res.set_header("Content-Type", "text/html");
     //     }
+    //     else
+    //     {
+    //         TODO: return user to shadow realm because authentication was unsuccesful
+    //     }
     // }
 
     // // close socket for KVS server
     // close(kvs_sock);
 }
 
-// function to handle logout requests
+/// @brief handles logout requests on /api/logout route
+/// @param req HttpRequest object
+/// @param res HttpResponse object
 void logout_handler(const HttpRequest &req, HttpResponse &res)
 {
-    (void) req;
-    (void) res;
+    (void)req;
+    (void)res;
 }
 
-// function to handle password update requests
+/// @brief handles password change requests on /api/pass_change route
+/// @param req HttpRequest object
+/// @param res HttpResponse object
 void update_password_handler(const HttpRequest &req, HttpResponse &res)
 {
-    (void) req;
-    (void) res;
+    (void)req;
+    (void)res;
 }
 
-// helper function that validates the password of a user
+/// @brief validates the password against the KVS given the username using cryptographically secure challenge-response protocol
+/// @param kvs_fd file descriptor for KVS server
+/// @param username username associated with current client session
+/// @param password password associated with current client session
+/// @return true if password is associated with valid user; false otherwise
 bool validate_password(int kvs_fd, std::string &username, std::string &password)
 {
     // construct row key
@@ -208,7 +243,9 @@ bool validate_password(int kvs_fd, std::string &username, std::string &password)
     }
 }
 
-// helper function for hashing using SHA256 algorithm
+/// @brief helper function generating 32-byte hash string using SHA256 cryptographic hash algorithm
+/// @param string string to be hashed
+/// @param outputBuffer buffer to store resulting hash
 void sha256(char *string, char outputBuffer[65])
 {
     unsigned char hash[SHA256_DIGEST_LENGTH];
@@ -221,7 +258,9 @@ void sha256(char *string, char outputBuffer[65])
     outputBuffer[64] = 0;
 }
 
-// helper function that generates random strings
+/// @brief helper function that generates a random challenge in the form of a string
+/// @param length length of the challenge to be generated
+/// @return returns the generated challenge
 std::string generate_challenge(std::size_t length)
 {
     const std::string CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -240,7 +279,8 @@ std::string generate_challenge(std::size_t length)
     return random_string;
 }
 
-// helper function that generates secure random session IDs
+/// @brief helper function that generates secure random session IDs
+/// @return returns the generated session ID
 std::string generate_sid()
 {
     std::random_device random_device;
