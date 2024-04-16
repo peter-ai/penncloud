8e1439522010840b5b8f2b264da979282b3ec6c7
aashok12 <aashok@seas.upenn.edu>
2024-04-16 11:32:53 -0400

    Drive upload file fixed for postman
    Signed-off-by: aashok12 <aashok@seas.upenn.edu>

--- front_end/src/drive.cc
+++ front_end/src/drive.cc
@@ -12,6 +12,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <algorithm>
+#include <vector>
 
 #include "../include/drive.h"
 #include "../../http_server/include/http_server.h"
@@ -60,6 +61,59 @@ bool kv_successful(const std::vector<char> &vec)
 
     // Check if the first three characters match the prefix
     return std::equal(prefix.begin(), prefix.end(), vec.begin());
+}
+
+// Function to split a vector<char> based on a vector<char> delimiter
+std::vector<std::vector<char>> split_vector(const std::vector<char> &data, const std::vector<char> &delimiter)
+{
+    std::vector<std::vector<char>> result;
+    size_t start = 0;
+    size_t end = data.size();
+
+    while (start < end)
+    {
+        // Find the next occurrence of delimiter starting from 'start'
+        auto pos = std::search(data.begin() + start, data.end(), delimiter.begin(), delimiter.end());
+
+        if (pos == data.end())
+        {
+            // No delimiter found, copy the rest of the vector
+            result.emplace_back(data.begin() + start, data.end());
+            break;
+        }
+        else
+        {
+            // Delimiter found, copy up to the delimiter and move 'start' past the delimiter
+            result.emplace_back(data.begin() + start, pos);
+            start = std::distance(data.begin(), pos) + delimiter.size();
+        }
+    }
+
+    return result;
+}
+
+// Function to split a vector<char> based on the first occurrence of a vector<char> delimiter
+std::vector<std::vector<char>> split_vec_first_delim(const std::vector<char> &data, const std::vector<char> &delimiter)
+{
+    std::vector<std::vector<char>> result;
+    size_t start = 0;
+
+    // Find the first occurrence of delimiter in data
+    auto pos = std::search(data.begin() + start, data.end(), delimiter.begin(), delimiter.end());
+
+    if (pos == data.end())
+    {
+        // No delimiter found, return the whole vector as a single part
+        result.emplace_back(data.begin(), data.end());
+    }
+    else
+    {
+        // Delimiter found, split at the delimiter
+        result.emplace_back(data.begin() + start, pos);          // Part before the delimiter
+        result.emplace_back(pos + delimiter.size(), data.end()); // Part after the delimiter
+    }
+
+    return result;
 }
 
 void open_filefolder(const HttpRequest &req, HttpResponse &res)
@@ -149,37 +203,72 @@ void upload_file(const HttpRequest &req, HttpResponse &res)
     if (!req.body_as_bytes().empty())
     {
 
-        // Copy request body to response body
-        std::vector<char> body_content = req.body_as_bytes();
-        res.append_body_bytes(body_content.data(), body_content.size());
+        // ------- Get file name and clean up to get file binary ----
+        // Get the single file uploaded
+        std::vector<std::string> headers;
 
-        std::cout << "Content-Disposition header" << std::endl;
-        std::vector<std::string> headers = req.get_header("Content-Disposition");
-        for (const auto &header : headers)
-        {
-            std::cout << header << std::endl;
-        }
-
-
-         std::cout << "Content-Type header" << std::endl;
+        // Find form boundary
         headers = req.get_header("Content-Type");
-        for (const auto &header : headers)
-        {
-            std::cout << header << std::endl;
-        }
+        std::string header_str(headers[0]);
+        // boundary provided by form
+        std::vector<std::string> find_boundary = Utils::split_on_first_delim(header_str, "boundary=");
+        std::vector<char> bound_vec(find_boundary.back().begin(), find_boundary.back().end());
 
-          // int sockfd = FeUtils::open_socket();
-           // std::vector<char> file_content = FeUtils::kv_get(sockfd, par, filename_vec);
+        std::vector<char> line_feed = {'\r', '\n'};
 
-        
+        // split body on boundary
+        std::vector<std::vector<char>> elements = split_vector(req.body_as_bytes(), bound_vec);
+        std::vector<char> elem1 = elements[1];
 
-        // Respond with a success message
-        std::string response_body = "File uploaded successfully";
+        // @note: assuming we only upload 1 file at a time?
+        std::vector<char> file_data = split_vec_first_delim(elem1, line_feed)[1];
+
+        // split file data to separate file metadata from binary values
+        std::vector<std::vector<char>> body_elems = split_vec_first_delim(file_data, line_feed);
+
+        // parse file headers to get name of file
+        std::string file_headers(body_elems[0].begin(), body_elems[0].end());
+        std::string content_disp = Utils::split_on_first_delim(file_headers, "\n")[0];
+        std::string filename_toparse = Utils::split_on_first_delim(file_headers, "filename=").back();
+
+        // file name string
+        std::string filename = Utils::split(filename_toparse, "\"")[0];
+
+        // get file binary
+        std::vector<char> file_binary = split_vec_first_delim(body_elems[1], line_feed)[1];
+        file_binary = split_vec_first_delim(file_binary, line_feed)[1];
+
+        // Get path of parent directory where we are appending
+
+        // path is /api/drive/upload/:parentpath where parent dir is the page that is being displayed
+        std::string parentpath_str = req.path.substr(18);
+        std::string childpath_str = parentpath_str + filename;
+
+        std::vector<char> row_vec(parentpath_str.begin(), parentpath_str.end());
+        std::vector<char> col_vec(childpath_str.begin(), childpath_str.end());
+
+
+        std::cout << "row: " <<parentpath_str << std::endl;
+        std::cout << "column: " <<childpath_str << std::endl;
+
+        // int sockfd = FeUtils::open_socket();
+
+        // std::vector<char> kvs_resp = FeUtils::kv_put(sockfd, row_vec, col_vec, file_binary);
+
+        // if (kv_successful(kvs_resp))
+        // {
+        //     // @todo should we instead get row for the page they are on?
+        //     res.set_code(200); // OK
+        //     // res.append_body_bytes(file_binary.data(), file_binary.size());
+        // } else {
+        //     res.set_code(400);
+        //     // maybe retry? tbd
+
+        // }
 
         // @todo should we instead get row for the page they are on?
         res.set_code(200); // OK
-
-
+        res.append_body_bytes(file_binary.data(), file_binary.size());
     }
     else
     {
