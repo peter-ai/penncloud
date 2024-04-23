ad88c5ea4bc8b52af9b5e108570205c8c88fad77
Bilal Ali <bilalal495@outlook.com>
2024-04-22 22:14:37 -0400

    Redefine header file for client communication with kvs

--- backend_server/include/kvs_client.h
+++ backend_server/include/kvs_client.h
@@ -7,15 +7,14 @@
 #include <unistd.h>     // close
 
 #include "tablet.h"
-#include "backend_server.h"
 #include "../../utils/include/utils.h"
 
 class KVSClient
 {
     // fields
 private:
-    int client_fd;   // client's bound fd
-    int client_port; // client's port
+    int client_fd;   // fd to communicate with client
+    int client_port; // Port that client is sending data from
     Logger kvs_client_logger;
 
     // methods
@@ -23,39 +22,18 @@ public:
     // client initialized with an associated file descriptor and client's port
     KVSClient(int client_fd, int client_port) : client_fd(client_fd), client_port(client_port),
                                                 kvs_client_logger("KVS Client [" + std::to_string(client_port) + "]"){};
-    // disable default constructor - Client should only be created with an associated fd
+    // disable default constructor - Client should only be created with an associated fd and port
     KVSClient() = delete;
 
-    void read_from_network(); // read data from network (thread function)
+    void read_from_client(); // read data from client
 
 private:
-    // read first 4 bytes from stream to get command and then call corresponding command
-    void handle_command(std::vector<char> &client_stream);
-
-    // remote-writes related methods
-    // Note that inputs is NOT passed by reference because a copy of inputs is needed
-    // a copy is necessary because the inputs passed in will be used by the primary to perform the command after confirmation that all secondaries are done
-    // if we take a reference and modify it, it'll modify the data that primary will use to perform the command after
-    int send_operation_to_secondaries(std::vector<char> inputs);
-    int wait_for_secondary_acks(std::vector<int> &secondary_fds); // loops and waits for secondaries to send acknowledgements
-    std::vector<char> forward_operation_to_primary(std::vector<char> &inputs);
-
-    // retrieve data tablet to service command
-    std::shared_ptr<Tablet> retrieve_data_tablet(std::string &row);
-
-    // send response to client
-    void send_response(std::vector<char> &response_msg);
-
-    // read only methods
-    std::vector<char> getr(std::vector<char> &inputs);
-    std::vector<char> getv(std::vector<char> &inputs);
-
-    // write methods
-    std::vector<char> call_write_command(std::string command, std::vector<char> &inputs);
-    std::vector<char> putv(std::vector<char> &inputs);
-    std::vector<char> cput(std::vector<char> &inputs);
-    std::vector<char> delr(std::vector<char> &inputs);
-    std::vector<char> delv(std::vector<char> &inputs);
+    void handle_command(std::vector<char> &client_stream);                     // read first 4 bytes from client stream and call corresponding command handler
+    std::vector<char> getr(std::vector<char> &inputs);                         // get row from tablet
+    std::vector<char> getv(std::vector<char> &inputs);                         // get value from tablet
+    std::vector<char> forward_operation_to_primary(std::vector<char> &inputs); // forward non-read operations received from client to primary
+    std::shared_ptr<Tablet> retrieve_data_tablet(std::string &row);            // retrieve data tablet to service read commands
+    void send_response(std::vector<char> &response_msg);                       // send response to client
 };
 
 #endif
