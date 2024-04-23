32b1561379a51305877e7531c9e6eec5976a44e0
aashok12 <aashok@seas.upenn.edu>
2024-04-22 22:22:29 -0400

    Validation and cookie refresh added
    Signed-off-by: aashok12 <aashok@seas.upenn.edu>

--- front_end/src/drive.cc
+++ front_end/src/drive.cc
@@ -5,16 +5,16 @@
  *      Author: aashok12
  */
 #include "../include/drive.h"
-
+using namespace std;
 // Folder handlers
 
-std::vector<char> ok_vec = {'+', 'O', 'K', ' '};
-std::vector<char> err_vec = {'-', 'E', 'R', ' '};
+vector<char> ok_vec = {'+', 'O', 'K', ' '};
+vector<char> err_vec = {'-', 'E', 'R', ' '};
 
 // helper to return parent path
-std::string split_parent_filename(const std::vector<std::string> &vec, std::string &filename)
+string split_parent_filename(const vector<string> &vec, string &filename)
 {
-    std::string parentpath;
+    string parentpath;
 
     // Iterate over all elements except the last one
     for (size_t i = 0; i < vec.size() - 1; ++i)
@@ -28,13 +28,19 @@ std::string split_parent_filename(const std::vector<std::string> &vec, std::stri
     return parentpath;
 }
 
+// gets username from file path
+string get_username(const string path)
+{
+    return Utils::split_on_first_delim(path, "/")[0];
+}
+
 // checks if path ends in /, if yes folder. Otherwise of type file
-bool is_folder(const std::vector<char> &vec)
+bool is_folder(const vector<char> &vec)
 {
     return vec.back() == '/';
 }
 
-bool kv_successful(const std::vector<char> &vec)
+bool kv_successful(const vector<char> &vec)
 {
     // Check if the vector has at least 3 characters
     if (vec.size() < 3)
@@ -43,16 +49,16 @@ bool kv_successful(const std::vector<char> &vec)
     }
 
     // Define the expected prefix
-    std::vector<char> prefix = {'+', 'O', 'K'};
+    vector<char> prefix = {'+', 'O', 'K'};
 
     // Check if the first three characters match the prefix
-    return std::equal(prefix.begin(), prefix.end(), vec.begin());
+    return equal(prefix.begin(), prefix.end(), vec.begin());
 }
 
 // Function to split a vector<char> based on a vector<char> delimiter
-std::vector<std::vector<char>> split_vector(const std::vector<char> &data, const std::vector<char> &delimiter)
+vector<vector<char>> split_vector(const vector<char> &data, const vector<char> &delimiter)
 {
-    std::vector<std::vector<char>> result;
+    vector<vector<char>> result;
     size_t start = 0;
     size_t end = data.size();
 
@@ -64,7 +70,7 @@ std::vector<std::vector<char>> split_vector(const std::vector<char> &data, const
     while (start < end)
     {
         // Find the next occurrence of delimiter starting from 'start'
-        auto pos = std::search(data.begin() + start, data.end(), delimiter.begin(), delimiter.end());
+        auto pos = search(data.begin() + start, data.end(), delimiter.begin(), delimiter.end());
 
         if (pos == data.end())
         {
@@ -76,7 +82,7 @@ std::vector<std::vector<char>> split_vector(const std::vector<char> &data, const
         {
             // Delimiter found, copy up to the delimiter and move 'start' past the delimiter
             result.emplace_back(data.begin() + start, pos);
-            start = std::distance(data.begin(), pos) + delimiter.size();
+            start = distance(data.begin(), pos) + delimiter.size();
         }
     }
 
@@ -84,13 +90,13 @@ std::vector<std::vector<char>> split_vector(const std::vector<char> &data, const
 }
 
 // Function to split a vector<char> based on the first occurrence of a vector<char> delimiter
-std::vector<std::vector<char>> split_vec_first_delim(const std::vector<char> &data, const std::vector<char> &delimiter)
+vector<vector<char>> split_vec_first_delim(const vector<char> &data, const vector<char> &delimiter)
 {
-    std::vector<std::vector<char>> result;
+    vector<vector<char>> result;
     size_t start = 0;
 
     // Find the first occurrence of delimiter in data
-    auto pos = std::search(data.begin() + start, data.end(), delimiter.begin(), delimiter.end());
+    auto pos = search(data.begin() + start, data.end(), delimiter.begin(), delimiter.end());
 
     if (pos == data.end())
     {
@@ -108,9 +114,9 @@ std::vector<std::vector<char>> split_vec_first_delim(const std::vector<char> &da
 }
 
 // helper to return parent path
-std::vector<char> format_folder_contents(std::vector<std::vector<char>> &vec)
+vector<char> format_folder_contents(vector<vector<char>> &vec)
 {
-    std::vector<char> output;
+    vector<char> output;
 
     if (vec.size() == 0)
     {
@@ -411,61 +417,95 @@ void open_filefolder(const HttpRequest &req, HttpResponse &res)
 void upload_file(const HttpRequest &req, HttpResponse &res)
 {
 
+    // Get path of parent directory where we are appending
+
+    // path is /api/drive/upload/:parentpath where parent dir is the page that is being displayed
+    string parentpath_str = req.path.substr(18);
+    string username = get_username(parentpath_str);
+
+    bool present = HttpServer::check_kvs_addr(username);
+    std::vector<std::string> kvs_addr;
+
+    // check if we know already know the KVS server address for user
+    if (present)
+    {
+        kvs_addr = HttpServer::get_kvs_addr(username);
+    }
+    // otherwise get KVS server address from coordinator
+    else
+    {
+        // query the coordinator for the KVS server address
+        kvs_addr = FeUtils::query_coordinator(username);
+    }
+
+    // create socket for communication with KVS server
+    int sockfd = FeUtils::open_socket(kvs_addr[0], std::stoi(kvs_addr[1]));
+
+    // validate session id
+    string valid_session_id = FeUtils::validate_session_id(sockfd, username, req);
+    // if invalid, return an error?
+    // @todo :: redirect to login page?
+    if (valid_session_id.empty())
+    {
+        // for now, returning code for check on postman
+        res.set_code(401);
+        close(sockfd);
+        return;
+    }
+
     // Check if the request contains a body
     if (!req.body_as_bytes().empty())
     {
 
         // ------- Get file name and clean up to get file binary ----
         // Get the single file uploaded
-        std::vector<std::string> headers;
+        vector<string> headers;
 
         // Find form boundary
         headers = req.get_header("Content-Type");
-        std::string header_str(headers[0]);
+        string header_str(headers[0]);
         // boundary provided by form
-        std::vector<std::string> find_boundary = Utils::split_on_first_delim(header_str, "boundary=");
-        std::vector<char> bound_vec(find_boundary.back().begin(), find_boundary.back().end());
+        vector<string> find_boundary = Utils::split_on_first_delim(header_str, "boundary=");
+        vector<char> bound_vec(find_boundary.back().begin(), find_boundary.back().end());
 
-        std::vector<char> line_feed = {'\r', '\n'};
+        vector<char> line_feed = {'\r', '\n'};
 
         // split body on boundary
-        std::vector<std::vector<char>> elements = split_vector(req.body_as_bytes(), bound_vec);
-        std::vector<char> elem1 = elements[1];
+        vector<vector<char>> elements = split_vector(req.body_as_bytes(), bound_vec);
+        vector<char> elem1 = elements[1];
 
         // @note: assuming we only upload 1 file at a time?
-        std::vector<char> file_data = split_vec_first_delim(elem1, line_feed)[1];
+        vector<char> file_data = split_vec_first_delim(elem1, line_feed)[1];
 
         // split file data to separate file metadata from binary values
-        std::vector<std::vector<char>> body_elems = split_vec_first_delim(file_data, line_feed);
+        vector<vector<char>> body_elems = split_vec_first_delim(file_data, line_feed);
 
         // parse file headers to get name of file
-        std::string file_headers(body_elems[0].begin(), body_elems[0].end());
-        std::string content_disp = Utils::split_on_first_delim(file_headers, "\n")[0];
-        std::string filename_toparse = Utils::split_on_first_delim(file_headers, "filename=").back();
+        string file_headers(body_elems[0].begin(), body_elems[0].end());
+        string content_disp = Utils::split_on_first_delim(file_headers, "\n")[0];
+        string filename_toparse = Utils::split_on_first_delim(file_headers, "filename=").back();
 
         // file name string
-        std::string filename = Utils::split(filename_toparse, "\"")[0];
+        string filename = Utils::split(filename_toparse, "\"")[0];
 
         // get file binary
-        std::vector<char> file_binary = split_vec_first_delim(body_elems[1], line_feed)[1];
+        vector<char> file_binary = split_vec_first_delim(body_elems[1], line_feed)[1];
         file_binary = split_vec_first_delim(file_binary, line_feed)[1];
 
-        // Get path of parent directory where we are appending
-
-        // path is /api/drive/upload/:parentpath where parent dir is the page that is being displayed
-        std::string parentpath_str = req.path.substr(18);
-        if (parentpath_str.back() != '/'){
+        if (parentpath_str.back() != '/')
+        {
             res.set_code(400);
+              // set cookies on response
+    FeUtils::set_cookies(res, username, valid_session_id);
+            close(sockfd);
             return;
         }
-        std::string childpath_str = parentpath_str + filename;
+        string childpath_str = parentpath_str + filename;
 
-        std::vector<char> row_vec(parentpath_str.begin(), parentpath_str.end());
-        std::vector<char> col_vec(filename.begin(), filename.end());
+        vector<char> row_vec(parentpath_str.begin(), parentpath_str.end());
+        vector<char> col_vec(filename.begin(), filename.end());
 
-        int sockfd = FeUtils::open_socket();
-
-        std::vector<char> kvs_resp = FeUtils::kv_put(sockfd, row_vec, col_vec, file_binary);
+        vector<char> kvs_resp = FeUtils::kv_put(sockfd, row_vec, col_vec, file_binary);
 
         if (kv_successful(kvs_resp))
         {
@@ -485,6 +525,11 @@ void upload_file(const HttpRequest &req, HttpResponse &res)
         // No body found in the request
         res.set_code(400); // Bad Request
     }
+
+    // set cookies on response
+    FeUtils::set_cookies(res, username, valid_session_id);
+
+    close(sockfd);
 }
 
 // creates a new folder
@@ -493,16 +538,47 @@ void create_folder(const HttpRequest &req, HttpResponse &res)
     // uses a post request to add a new folder to the current parent directory.
 
     // path is /api/drive/create/:parentpath where parent dir is the page that is being displayed
-    std::string parentpath_str = req.path.substr(18);
+    string parentpath_str = req.path.substr(18);
 
-    std::string req_body = req.body_as_string();
+    string username = get_username(parentpath_str);
+    bool present = HttpServer::check_kvs_addr(username);
+    std::vector<std::string> kvs_addr;
+
+    // check if we know already know the KVS server address for user
+    if (present)
+    {
+        kvs_addr = HttpServer::get_kvs_addr(username);
+    }
+    // otherwise get KVS server address from coordinator
+    else
+    {
+        // query the coordinator for the KVS server address
+        kvs_addr = FeUtils::query_coordinator(username);
+    }
+
+    // create socket for communication with KVS server
+    int sockfd = FeUtils::open_socket(kvs_addr[0], std::stoi(kvs_addr[1]));
+
+    // validate session id
+    string valid_session_id = FeUtils::validate_session_id(sockfd, username, req);
+    // if invalid, return an error?
+    // @todo :: redirect to login page?
+    if (valid_session_id.empty())
+    {
+        // for now, returning code for check on postman
+        res.set_code(401);
+        close(sockfd);
+        return;
+    }
+
+    string req_body = req.body_as_string();
 
     // check that ody is not empty
     if (!req_body.empty())
     {
         // get name of folder
-        std::string key = "name=";
-        std::vector<std::string> elements = Utils::split_on_first_delim(req_body, key);
+        string key = "name=";
+        vector<string> elements = Utils::split_on_first_delim(req_body, key);
 
         // if key doesn't exist, return 400
         if (elements.size() < 1)
@@ -511,20 +587,17 @@ void create_folder(const HttpRequest &req, HttpResponse &res)
             return;
         }
 
-        std::vector<char> folder_name(elements[0].begin(), elements[0].end());
+        vector<char> folder_name(elements[0].begin(), elements[0].end());
         folder_name.push_back('/');
-        std::vector<char> row_name(parentpath_str.begin(), parentpath_str.end());
+        vector<char> row_name(parentpath_str.begin(), parentpath_str.end());
 
-        // check if folder name is taken
-        int sockfd = FeUtils::open_socket();
-
-        std::vector<char> folder_content = FeUtils::kv_get_row(sockfd, row_name);
+        vector<char> folder_content = FeUtils::kv_get_row(sockfd, row_name);
 
         // content list, remove '+OK<sp>'
-        std::vector<char> folder_elements(folder_content.begin() + 4, folder_content.end());
+        vector<char> folder_elements(folder_content.begin() + 4, folder_content.end());
         // split on delim
-        std::vector<std::vector<char>> contents = split_vector(folder_elements, {'\b'});
-        std::vector<char> formatted_content = format_folder_contents(contents);
+        vector<vector<char>> contents = split_vector(folder_elements, {'\b'});
+        vector<char> formatted_content = format_folder_contents(contents);
 
         // if folder name in use
         if (contains_subseq(formatted_content, folder_name))
@@ -538,15 +611,15 @@ void create_folder(const HttpRequest &req, HttpResponse &res)
             {
 
                 // create new column for row
-                std::vector<char> folder_row = row_name;
+                vector<char> folder_row = row_name;
                 folder_row.insert(folder_row.end(), folder_name.begin(), folder_name.end());
-                std::vector<char> kvs_resp = FeUtils::kv_put(sockfd, folder_row, {}, {});
+                vector<char> kvs_resp = FeUtils::kv_put(sockfd, folder_row, {}, {});
 
                 // get parent folder to show that this folder has been nested
                 folder_content = FeUtils::kv_get_row(sockfd, row_name);
 
                 // content list, remove '+OK<sp>'
-                std::vector<char> folder_elements(folder_content.begin() + 4, folder_content.end());
+                vector<char> folder_elements(folder_content.begin() + 4, folder_content.end());
                 contents = split_vector(folder_elements, {'\b'});
                 formatted_content = format_folder_contents(contents);
                 res.append_body_bytes(formatted_content.data(), formatted_content.size());
@@ -558,12 +631,16 @@ void create_folder(const HttpRequest &req, HttpResponse &res)
                 res.set_code(400);
             }
         }
-        close(sockfd);
     }
     else
     {
         res.set_code(400);
     }
+
+      // set cookies on response
+    FeUtils::set_cookies(res, username, valid_session_id);
+
+    close(sockfd);
 }
 
 // deletes file or folder
@@ -572,21 +649,51 @@ void delete_filefolder(const HttpRequest &req, HttpResponse &res)
 {
 
     // of type /api/drive/delete/* where child directory is being served
-    std::string childpath_str = req.path.substr(18);
-    std::vector<char> child_path(childpath_str.begin(), childpath_str.end());
+    string childpath_str = req.path.substr(18);
+    string username = get_username(childpath_str);
+    bool present = HttpServer::check_kvs_addr(username);
+    std::vector<std::string> kvs_addr;
+
+    // check if we know already know the KVS server address for user
+    if (present)
+    {
+        kvs_addr = HttpServer::get_kvs_addr(username);
+    }
+    // otherwise get KVS server address from coordinator
+    else
+    {
+        // query the coordinator for the KVS server address
+        kvs_addr = FeUtils::query_coordinator(username);
+    }
+
+    // create socket for communication with KVS server
+    int sockfd = FeUtils::open_socket(kvs_addr[0], std::stoi(kvs_addr[1]));
+
+    // validate session id
+    string valid_session_id = FeUtils::validate_session_id(sockfd, username, req);
+    // if invalid, return an error?
+    // @todo :: redirect to login page?
+    if (valid_session_id.empty())
+    {
+        // for now, returning code for check on postman
+        res.set_code(401);
+        close(sockfd);
+        return;
+    }
+
+    vector<char> child_path(childpath_str.begin(), childpath_str.end());
 
     // if we are trying to delete a file
     if (!is_folder(child_path))
     {
         // get file name
-        std::string filename;
-        std::string parentpath_str = split_parent_filename(Utils::split(childpath_str, "/"), filename);
+        string filename;
+        string parentpath_str = split_parent_filename(Utils::split(childpath_str, "/"), filename);
 
         // comver tto vector<char>
-        std::vector<char> parent_path_vec(parentpath_str.begin(), parentpath_str.end());
-        std::vector<char> filename_vec(filename.begin(), filename.end());
+        vector<char> parent_path_vec(parentpath_str.begin(), parentpath_str.end());
+        vector<char> filename_vec(filename.begin(), filename.end());
 
-        int sockfd = FeUtils::open_socket();
 
         if (kv_successful(FeUtils::kv_del(sockfd, parent_path_vec, filename_vec)))
         {
@@ -594,16 +701,24 @@ void delete_filefolder(const HttpRequest &req, HttpResponse &res)
             res.set_code(200);
 
             // reload page to show file has been deleted
-            std::vector<char> folder_content = FeUtils::kv_get_row(sockfd, parent_path_vec);
+            vector<char> folder_content = FeUtils::kv_get_row(sockfd, parent_path_vec);
 
             // content list, remove '+OK<sp>'
-            std::vector<char> folder_elements(folder_content.begin() + 4, folder_content.end());
-            std::vector<std::vector<char>> contents = split_vector(folder_elements, {'\b'});
-            std::vector<char> formatted_content = format_folder_contents(contents);
+            vector<char> folder_elements(folder_content.begin() + 4, folder_content.end());
+            vector<vector<char>> contents = split_vector(folder_elements, {'\b'});
+            vector<char> formatted_content = format_folder_contents(contents);
             res.append_body_bytes(formatted_content.data(), formatted_content.size());
             res.set_code(200);
-        } else {
+        }
+        else
+        {
             res.set_code(400);
         }
+
     }
+
+      // set cookies on response
+    FeUtils::set_cookies(res, username, valid_session_id);
+
+    close(sockfd);
 }
