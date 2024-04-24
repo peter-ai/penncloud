50a02d8b5a6b64e27e4f0780fb32c6adf2af9eaf
Bilal Ali <bilalal495@outlook.com>
2024-04-24 02:50:13 -0400

    Add more documentation to tablet class

--- backend_server/include/tablet.h
+++ backend_server/include/tablet.h
@@ -12,56 +12,42 @@ class Tablet
 {
     // fields
 public:
-    // NOTE: if the key range is "aa" to "bz", this tablet will manage every key UP TO AND INCLUDING "bz"
-    // For example, a key called bzzzz would be managed in this server. The next server would start at "ca"
-    std::string range_start; // start of key range managed by this backend server
-    std::string range_end;   // end of key range managed by this backend server
+    std::string range_start; // start of key range managed by this tablet
+    std::string range_end;   // end of key range managed by this tablet
+    Logger tablet_logger;
 
 private:
-    static const char delimiter;
-    static const std::string ok;
-    static const std::string err;
+    static const char delimiter;  // delimiter used to separate components of tablet command
+    static const std::string ok;  // "+OK" (for success messages)
+    static const std::string err; // "-ER" (for error messages)
 
-    // In memory representation of a tablet's data
-    // Map of row key (string) -> column key (string) -> value (vector<char>)
-    std::map<std::string, std::unordered_map<std::string, std::vector<char>>> data;
-
-    // enables row level locking
-    // read-write locking (a row has concurrent read access, exclusive write access)
-    std::unordered_map<std::string, std::shared_timed_mutex> row_locks;
-
-    // locks access to the row_locks map while a new lock is being created for a row
-    std::shared_timed_mutex row_locks_mutex;
+    // row key (string) -> column key (string) -> value (vector<char>)
+    std::map<std::string, std::unordered_map<std::string, std::vector<char>>> data; // In-memory representation of a tablet
+    std::unordered_map<std::string, std::shared_timed_mutex> row_locks;             // row-level read-write locks for tablet data
+    std::shared_timed_mutex row_locks_mutex;                                        // read-write lock for row_locks map
 
     // methods
 public:
-    // Constructor to initialize a tablet
+    // Constructor to initialize a tablet - used on server start up
     Tablet(std::string range_start, std::string range_end)
-        : range_start(range_start), range_end(range_end), data(), row_locks(), row_locks_mutex() {}
+        : range_start(range_start), range_end(range_end), data(), row_locks(), row_locks_mutex(), tablet_logger("Tablet " + range_start + ":" + range_end) {}
 
-    // default constructor
-    // NOTE: this should ONLY be used during deserialization
-    // to deserialize, create a tablet using this constructor and then call deserialize on the instance to populate the object's fields
-    // deserialize will populate the tablet's fields
-    Tablet() : range_start(""), range_end(""), data(), row_locks(), row_locks_mutex() {}
+    // Default constructor
+    // This should ONLY be used when deserializing a file into a tablet.
+    // To deserialize a file, initialize a tablet using this default constructor, then call deserialize on the tablet to populate its fields
+    Tablet() : range_start(""), range_end(""), data(), row_locks(), row_locks_mutex(), tablet_logger("Tablet") {}
 
     /**
-     *  Methods for interacting with in memory data
+     *  READ-ONLY METHODS
      */
 
-    // read all columns from tablet data
-    std::vector<char> get_row(std::string &row);
+    std::vector<char> get_all_rows();                                // read all rows in tablet (rows are separated by delimiter)
+    std::vector<char> get_row(std::string &row);                     // read all columns from tablet data (columns are separated by delimiter)
+    std::vector<char> get_value(std::string &row, std::string &col); // read value from tablet data
 
-    // read value from tablet data
-    std::vector<char> get_value(std::string &row, std::string &col);
-
-    // acquire exclusive lock on row
-    // this should precede ALL write operations and should be called when responding to PREPARE command
-    int acquire_exclusive_row_lock(std::string &operation, std::string &row);
-
-    // release exclusive lock on row
-    // this should only be called during an ABORT operation, since write didn't actually occur
-    void release_exclusive_row_lock(std::string &row);
+    /**
+     *  WRITE METHODS
+     */
 
     // add value at supplied row and column to tablet data
     // this operation acquires exclusive access to the row
@@ -85,9 +71,20 @@ public:
     std::vector<char> cond_put_value(std::string &row, std::string &col, std::vector<char> &curr_val, std::vector<char> &new_val);
 
     /**
-     * Serialization methods
+     * ACQUIRING/RELEASING LOCKS FOR WRITE METHODS
      */
-    // ! note for serialization - don't need to serialize row locks, since we can construct one for each row on deserialization
+
+    // This operation MUST precede ALL write operations
+    // Usage - called by primary server before sending PREP command, and by secondary server in response to PREP command
+    int acquire_exclusive_row_lock(std::string &operation, std::string &row); // acquires shared lock on row_locks, exclusive lock on row
+
+    // This operation is ONLY called during an ABRT operation
+    // Write methods automatically release their locks upon completion of the write operation
+    void release_exclusive_row_lock(std::string &row); // releases shared lock on row_locks and exclusive lock on row
+
+    /**
+     * SERIALIZATION METHODS
+     */
     void serialize(const std::string &file_name);   // serialize tablet into a file called file_name
     void deserialize(const std::string &file_name); // deserialize file_name into this tablet object
 
